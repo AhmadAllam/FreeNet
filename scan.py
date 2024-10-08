8e581dc5ce2b5307715815eb8cbbaad258511b91
Ahmad Allam <86953062+AhmadAllam@users.noreply.github.com>
2024-10-08 11:41:41 +0300

    update v4

--- scan.py
+++ scan.py
@@ -1,4 +1,4 @@
-# color settings
+# Color settings
 CN = "\033[K"
 Y1 = "\033[33m"
 RED = "\033[31m"
@@ -8,13 +8,14 @@ CC = "\033[0m"
 # Scan settings
 DEFAULT_DEEP = 2
 DEFAULT_MODE = "direct"
-DEFAULT_PORT = 80
+DEFAULT_PORTS = [80]
 DEFAULT_THREADS = 8
 DEFAULT_IGNORE_REDIRECT_LOCATION = ""
 DEFAULT_METHOD = "HEAD"
 DEFAULT_PROXY = None
 
-INJECT_SERVER_NAME_INDICATION = "twitter.com"
+# Inject settings
+INJECT_SERVER_NAME_INDICATION = "facebook.com"
 INJECT_TIMEOUT = 5
 INJECT_SHOW_LOG = False
 
@@ -32,6 +33,9 @@ import threading
 import signal
 
 lock = threading.RLock()
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def get_value_from_list(data, index, default=""):
     return data[index] if index < len(data) else default
@@ -51,33 +55,6 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 class BugScanner:
-    brainfuck_config = {
-        "ProxyRotator": {"Port": "3080"},
-        "Inject": {
-            "Enable": True,
-            "Type": 2,
-            "Port": "8989",
-            "Rules": {},
-            "Payload": "",
-            "MeekType": 0,
-            "ServerNameIndication": "twitter.com",
-            "Timeout": 5,
-            "ShowLog": False,
-        },
-        "PsiphonCore": 4,
-        "Psiphon": {
-            "CoreName": "psiphon-tunnel-core",
-            "Tunnel": 1,
-            "Region": "",
-            "Protocols": [
-                "FRONTED-MEEK-HTTP-OSSH",
-                "FRONTED-MEEK-OSSH",
-            ],
-            "TunnelWorkers": 6,
-            "KuotaDataLimit": 4,
-            "Authorizations": [""],
-        },
-    }
     scanned = {"direct": {}, "ssl": {}, "proxy": {}}
 
     def request(self, method, hostname, port, proxy=None):
@@ -102,17 +79,19 @@ class BugScanner:
             return self.request(method, hostname, port, {"http": f"http://{proxy}", "https": f"http://{proxy}"})
         return self.request(method, hostname, port)
 
-    def get_direct_response(self, method, hostname, port):
-        if f"{hostname}:{port}" in self.scanned["direct"]:
-            return None
-        response = self.get_response(method, hostname, port)
-        if response:
-            self.scanned["direct"][f"{hostname}:{port}"] = {
-                "status_code": response.status_code,
-                "server": response.headers.get("server", ""),
-            }
-            return self.scanned["direct"][f"{hostname}:{port}"]
-        return None
+    def get_direct_response(self, method, hostname, ports):
+        results = {}
+        for port in ports:
+            if f"{hostname}:{port}" in self.scanned["direct"]:
+                continue
+            response = self.get_response(method, hostname, port)
+            if response:
+                self.scanned["direct"][f"{hostname}:{port}"] = {
+                    "status_code": response.status_code,
+                    "server": response.headers.get("server", ""),
+                }
+                results[port] = self.scanned["direct"][f"{hostname}:{port}"]
+        return results
 
     def get_sni_response(self, hostname, deep):
         server_name_indication = ".".join(hostname.split(".")[0 - deep:])
@@ -122,9 +101,8 @@ class BugScanner:
             self.scanned["ssl"][server_name_indication] = None
         try:
             log_replace(server_name_indication)
-            socket_client = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2).wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_hostname=server_name_indication)
+            socket_client = ssl.create_default_context().wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM), server_hostname=server_name_indication)
             socket_client.settimeout(5)
-            socket_client.connect(("httpbin.org", 443))
             response = {"status": "True", "server_name_indication": server_name_indication}
         except:
             response = {"status": "", "server_name_indication": server_name_indication}
@@ -132,77 +110,37 @@ class BugScanner:
             self.scanned["ssl"][server_name_indication] = response
             return response
 
-    def get_proxy_response(self, method, hostname, port, proxy):
-        if f"{hostname}:{port}" in self.scanned["proxy"]:
-            return None
-        response = self.get_response(method, hostname, port, proxy)
-        if response and response.headers.get("location") != self.ignore_redirect_location:
-            self.scanned["proxy"][f"{hostname}:{port}"] = {
-                "proxy": proxy,
-                "method": method,
-                "url": response.url,
-                "status_code": response.status_code,
-                "headers": response.headers,
-            }
-            return self.scanned["proxy"][f"{hostname}:{port}"]
-        return None
-
     def print_result(self, host, hostname, port=None, status_code=None, server=None, sni=None):
         color = Y1
         server_str = f"{server:<20}" if server else " " * 20
         status_code_str = f"{status_code:<4}" if status_code else " " * 4
         hostname_str = f"{hostname}" if hostname else " "
-        
-        if server and server.lower().startswith("cloud"):
+
+        if server and "cloud" in server.lower():
             color = GREEN
-            with open("cloud.txt", "a+") as f:
-                f.seek(0)
-                if hostname not in f.read():
-                    f.write(f"{hostname}\n")
-        elif server and server.lower().startswith("apache"):
-            with open("apache.txt", "a+") as f:
-                f.seek(0)
-                if hostname not in f.read():
-                    f.write(f"{hostname}\n")
-        elif server and server.lower().startswith("nginx"):
-            with open("nginx.txt", "a+") as f:
-                f.seek(0)
-                if hostname not in f.read():
-                    f.write(f"{hostname}\n")
-        elif server and server.lower().startswith("awselb"):
-            with open("awselb.txt", "a+") as f:
+            with open("BugHosts/cloud_hosts.txt", "a+") as f:  # Save in BugHosts directory
                 f.seek(0)
                 if hostname not in f.read():
                     f.write(f"{hostname}\n")
         else:
-            with open("other.txt", "a+") as f:
+            with open("BugHosts/other_hosts.txt", "a+") as f:  # Save in BugHosts directory
                 f.seek(0)
                 if hostname not in f.read():
                     f.write(f"{hostname}\n")
 
         log(f"{color}{host:<15} {status_code_str} {server_str} {f'  {sni:<4}' if sni else '    '}  {hostname_str}{CC}")
 
-    def print_result_proxy(self, response):
-        if response is None:
-            return
-        data = [f"{response['proxy']} -> {response['method']} {response['url']} ({response['status_code']})"]
-        data.extend([f"|   {key}: {val}" for key, val in response['headers'].items()])
-        log("\n".join(data) + "\n")
-
     def scan(self):
         while True:
             for host, hostname in self.resolve(self.queue_hostname.get()):
                 if self.mode == "direct":
-                    response = self.get_direct_response(self.method, hostname, self.port)
-                    if response:
-                        self.print_result(host, hostname, port=self.port, status_code=response["status_code"], server=response["server"])
+                    responses = self.get_direct_response(self.method, hostname, self.ports)
+                    for port, response in responses.items():
+                        self.print_result(host, hostname, port=port, status_code=response["status_code"], server=response["server"])
                 elif self.mode == "ssl":
                     response = self.get_sni_response(hostname, self.deep)
                     if response:
                         self.print_result(host, response["server_name_indication"], sni=response["status"])
-                elif self.mode == "proxy":
-                    response = self.get_proxy_response(self.method, hostname, self.port, self.proxy)
-                    self.print_result_proxy(response)
             self.queue_hostname.task_done()
 
     def start(self, hostnames):
@@ -224,43 +162,38 @@ class BugScanner:
 
         self.queue_hostname.join()
 
-        if self.output is not None and self.mode == "direct" and len(self.brainfuck_config["Inject"]["Rules"]):
-            if os.name == "nt":
-                self.brainfuck_config["Psiphon"]["CoreName"] += ".exe"
-            with open(f"config.{self.output}.json", 'w', encoding='utf-8') as f:
-                dump = json.dumps(self.brainfuck_config, indent=4, ensure_ascii=False)
-                data = re.sub('\n +', lambda match: '\n' + '\t' * int((len(match.group().strip('\n')) / 4)), dump)
-                f.write(data)
-
 def main():
+    clear_screen()
     parser = argparse.ArgumentParser(formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=52))
-    parser.add_argument("-d", "--deep", help="subdomain deep", dest="deep", type=int, default=DEFAULT_DEEP)
+    parser.add_argument("-d", "--deep", help="subdomain depth", dest="deep", type=int, default=DEFAULT_DEEP)
     parser.add_argument("-m", "--mode", help="direct, proxy, ssl", dest="mode", type=str, default=DEFAULT_MODE)
-    parser.add_argument("-o", "--output", help="output file name", dest="output", type=str)
-    parser.add_argument("-p", "--port", help="target port", dest="port", type=int, default=DEFAULT_PORT)
+    parser.add_argument("-f", "--file", help="input file name", dest="output", type=str, default="BugHosts/All_Hosts.txt")  # Specified path
+    parser.add_argument("-p", "--ports", help="target ports (comma separated)", dest="ports", type=str, default="80")
     parser.add_argument("-t", "--threads", help="threads", dest="threads", type=int, default=DEFAULT_THREADS)
     parser.add_argument("-I", "--ignore-redirect-location", help="ignore redirect location for --mode proxy", dest="ignore_redirect_location", type=str, default=DEFAULT_IGNORE_REDIRECT_LOCATION)
-    parser.add_argument("-M", "--method", help="http method", dest="method", type=str, default=DEFAULT_METHOD)
+    parser.add_argument("-M", "--method", help="HTTP method", dest="method", type=str, default=DEFAULT_METHOD)
     parser.add_argument("-P", "--proxy", help="proxy.example.com:8080", dest="proxy", type=str)
 
     args = parser.parse_args()
-    if args.mode == "proxy" and not args.proxy:
-        parser.print_help()
-        return
-
+    
     bugscanner = BugScanner()
     bugscanner.deep = args.deep
     bugscanner.ignore_redirect_location = args.ignore_redirect_location
     bugscanner.method = args.method.upper()
     bugscanner.mode = args.mode
     bugscanner.output = args.output
-    bugscanner.port = args.port
+    bugscanner.ports = list(map(int, args.ports.split(',')))
     bugscanner.proxy = args.proxy
     bugscanner.threads = args.threads
     
-    with open("host.txt") as f:
-        hostnames = f.read().splitlines()
-    
+    # Read hostnames from the specified file
+    if os.path.exists(bugscanner.output):
+        with open(bugscanner.output) as f:
+            hostnames = f.read().splitlines()
+    else:
+        print(f"File {bugscanner.output} does not exist.")
+        return
+
     bugscanner.start(hostnames)
 
 if __name__ == "__main__":
